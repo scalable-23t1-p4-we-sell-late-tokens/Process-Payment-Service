a1e944a787531c6441f2d1633d48ff7af3fa959d
Andrew-Stuber <andrewstuber20@gmail.com>
2023-12-05 03:08:21 +0700

    added observibility

--- src/main/java/com/scalable/payment/controller/PaymentController.java
+++ src/main/java/com/scalable/payment/controller/PaymentController.java
@@ -1,10 +1,18 @@
 package com.scalable.payment.controller;
+
 import com.scalable.payment.model.Payment;
 import com.scalable.payment.service.PaymentService;
+
+import io.micrometer.core.instrument.MeterRegistry;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("payment")
@@ -12,10 +20,22 @@ public class PaymentController {
     @Autowired
     PaymentService paymentService;
 
+    private final MeterRegistry registry;
+
+    public PaymentController(MeterRegistry registry) {
+        this.registry = registry;
+    }
+
+    private final Logger LOG = LoggerFactory.getLogger(PaymentController.class);
+
     @PostMapping("/create-default/{username}")
     public ResponseEntity<String> createNewDefaultPayment(@PathVariable String username)
     {
         paymentService.createDefaultPayment(username);
+
+        registry.counter("creditUsername.total", "username", username).increment();
+        LOG.info("New user credit " + username + " successfully created");
+
         return ResponseEntity.ok().build();
     }
 
